58549b08bf053a15b811b1c8fef18614e2dbffbe
Steven Schreiber <sschreiber.ssteven@gmail.com>
2020-05-03 17:31:13 -0700

    Adding HTML from BootStrap to layout.html. Jeremy - added formatting to the abbout page.

--- flaskMain.py
+++ flaskMain.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Steven Schreiber',
+        'title': 'Event Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jeremy Meharg',
+        'title': 'Event Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
