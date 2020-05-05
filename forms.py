67f0e14ea6dd9290ea35d4cca9fb2efcfa5b0978
Steven Schreiber <sschreiber.ssteven@gmail.com>
2020-05-04 19:26:51 -0700

    Fixing typo in forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtfforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -20,4 +20,3 @@ class LoginForm(FlaskForm):
                               validators=[DataRequired(), Length(min=5, max=20)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    
