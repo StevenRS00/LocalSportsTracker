0a75f823078f9d64519e2d846084d49482a7bceb
Steven Schreiber <sschreiber.ssteven@gmail.com>
2020-05-06 22:12:21 -0700

    Changed to PEP8

--- localsportstracker/forms.py
+++ localsportstracker/forms.py
@@ -16,15 +16,16 @@ class RegistrationForm(FlaskForm):
   submit = SubmitField('Sign Up')
 
   def validate_username(self, username):
-      user = User.query.filter_by(username=username.data).first()
-      if user:
-          raise ValidationError('That username is taken already. Please choose another.')
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+      raise ValidationError(
+          'That username is taken already. Please choose another.')
 
   def validate_email(self, email):
-      user = User.query.filter_by(email=email.data).first()
-      if user:
-          raise ValidationError('That email is taken already. Please choose another.')
-
+    user = User.query.filter_by(email=email.data).first()
+    if user:
+      raise ValidationError(
+          'That email is taken already. Please choose another.')
 
 
 class LoginForm(FlaskForm):
