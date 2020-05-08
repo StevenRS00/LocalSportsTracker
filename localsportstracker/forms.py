a50910fd30987c8689e553498fb4996c5da41975
Steven Schreiber <sschreiber.ssteven@gmail.com>
2020-05-07 18:56:42 -0700

    Added environment variablels for sender email Jeremy - Added password reset related htmls, set sending email functionality, allowed passwords to be reset.

--- localsportstracker/forms.py
+++ localsportstracker/forms.py
@@ -60,3 +60,24 @@ class UpdateAccountForm(FlaskForm):
         if user:
           raise ValidationError(
               'That email is taken already. Please choose another.')
+
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+      user = User.query.filter_by(email=email.data).first()
+      if user is None:
+        raise ValidationError(
+            'There is no account with that email. Please create an account first.')
+
+class ResetPasswordForm(FlaskForm):
+     password = PasswordField('Password',
+                                validators=[DataRequired(), Length(min=5, max=20)])
+     confirm_password = PasswordField('Confirm Password',
+                                        validators=[DataRequired(), EqualTo('password')])
+
+     submit = SubmitField('Reset Password')
--- localsportstracker/routes.py
+++ localsportstracker/routes.py
@@ -2,10 +2,11 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
-from localsportstracker import app, db, bcrypt
-from localsportstracker.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from localsportstracker import app, db, bcrypt, mail
+from localsportstracker.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from localsportstracker.models import User, Event
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 posts = [
     {
@@ -47,7 +48,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash(
-            f'Your account has been created {form.username.data}!', 'success')
+            f'Your account has been created {form.username.data}! You can now login', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
@@ -105,3 +106,44 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form )
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', sender = 'noreply@gmail.com', recipients = [user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been set with instuctions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('This is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You can now login', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password',form = form)
